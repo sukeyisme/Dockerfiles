857d90bcbf9809cade9b93d9075ee26283aeac20
hodge shen <hodge.shen@gmail.com>
2018-10-05 19:39:15 +0800

    ..

---
+++ pgy/start.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/expect
+
+set user 22264820:003
+set pwd 123456
+
+
+spawn pgyvpn
+expect "Please Enter Your Account:"
+send "$user\r"
+expect "Please Enter Your Password:"
+send "$pwd\r"
+expect "Auto Login"
+send "n\r"
+expect "Login Successful"
+interact
